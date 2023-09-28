8d7546f62fc2815d087a68d31b580160c85c6c4f
abisheik88 <abishekvijayarangan@gmail.com>
2023-09-28 12:39:58 +0530

    file upload

---
+++ src/app.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+const fs = require('fs');
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    fs.readFile(file.buffer, 'utf-8', (err, data) => {
+      // if (err) {
+      //   console.error(err);
+      //   return;
+      // }
+      console.log(data);
+    });
+  }
+}
